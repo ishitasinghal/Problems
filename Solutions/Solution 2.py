a3af33dabab6fc47fe9e23b6967263b9f87e3000
Ishita Singhal <ishitasinghal08@gmail.com>
2020-08-02 16:47:51 +0530

    Extra line being printed!
    The pattern is correct but an extra line is being printed.

---
+++ Solutions/Solution 2.py
@@ -0,0 +1,15 @@
+def gf(n):
+    for i in range(1,n+2):
+        for j in range(i):
+            print("*", end='')
+        for k in range((2*n)-(2*j)):
+            print("#", end='')
+        for l in range(i):
+            print("*",end='')
+        print()
+    
+testcases = int(input())
+for _ in range(testcases):
+    num = int(input())
+    gf(num)
+    print()
